a41e2c2a525b43533c41f1ab109311f6d9f752f4
DimCore3 <3dmi3y@gmail.com>
2023-05-17 23:09:31 +0400

    добавил кнопку меню контента хэдера, блок контента, стили поправил и анимацию открытия меню.

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import classes from './index.module.scss';
 import LogoWithText from 'entities/logoWithText';
 import { PhoneMapInfoAllBranch, PhoneMapInfoCallOrder, SocialMediaExternalLinks } from 'features';
 
 const Header = () => {
+    const [isOpenMenu, setIsOpenMenu] = useState(false);
+
     return (
         <header className={classes.main_header}>
-            <LogoWithText />
-            <PhoneMapInfoAllBranch data='Воронеж, Монтажный проезд, 7'/>
-            <PhoneMapInfoCallOrder data='+7(999)988-64-11'/>
-            <SocialMediaExternalLinks />
+            <div className={classes.header_upper}>
+                <LogoWithText />
+                {!isOpenMenu &&
+                    <>
+                        <PhoneMapInfoAllBranch data='Воронеж, Монтажный проезд, 7' />
+                        <PhoneMapInfoCallOrder data='+7(999)988-64-11' />
+                        <SocialMediaExternalLinks />
+                    </>
+                }
+                <button onClick={() => setIsOpenMenu(!isOpenMenu)}>|||</button>
+            </div>
+            {isOpenMenu && <div className={classes.menu_container}> text text text </div>}
         </header>
     );
 }
